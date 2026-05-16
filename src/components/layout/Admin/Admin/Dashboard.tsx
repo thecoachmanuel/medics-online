0c4b7103a20f8484a5d38cfedc6f323abed41358
Apple Macbook Pro <admin@ADMINs-MacBook-Pro.local>
2026-05-16 21:39:03 +0100

    style: enhance Latest Booking UI with slot time and perfect circle profile pictures

--- src/components/layout/Admin/Admin/Dashboard.tsx
+++ src/components/layout/Admin/Admin/Dashboard.tsx
@@ -56,10 +56,10 @@ const Dashboard = () => {
           <div className="pt-4 border border-t-0">
             {dashData.latestAppointments.slice(0, 5).map((item: IAppointment, index: number) => (
               <div className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100" key={index}>
-                <img className="rounded-full w-10" src={item.docData.image} alt="" />
+                <img className="rounded-full w-10 h-10 object-cover border" src={item.docData.image} alt="" />
                 <div className="flex-1 text-sm">
                   <p className="text-gray-800 font-medium">{item.docData.name}</p>
-                  <p className="text-gray-600 ">Booking on {slotDateFormat(item.slotDate)}</p>
+                  <p className="text-gray-600 ">Booking on {slotDateFormat(item.slotDate)} at {item.slotTime}</p>
                 </div>
                 {item.cancelled ? (
                   <p className="text-red-400 text-xs font-medium">Cancelled</p>
